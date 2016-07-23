de7f74de744a7e0078d3b772ff2952098c60cfce
pitawan <pitawanpor@gmail.com>
2016-07-23 14:01:46 +0700

    Create Methodแล้ว

--- app/src/main/java/tw/pitawanpor/twschool/MainActivity.java
+++ app/src/main/java/tw/pitawanpor/twschool/MainActivity.java
@@ -2,6 +2,8 @@ package tw.pitawanpor.twschool;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.internal.view.ViewPropertyAnimatorCompatSet;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }   // Main Method
+
+    //Get Event from Click
+    public void clickSignUpMain(View view) {
+
     }
-}
+}   // Main Class นี่คือคลาสหลัก
